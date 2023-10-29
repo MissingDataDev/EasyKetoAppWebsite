fa27980952e079ce7919e3072ea2695c5467c1c9
KetoArchitect <easyketoapp@googlemail.com>
2023-10-29 02:42:43 +0100

    Fix next bug

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image 
+      <img 
         src="/assets/images/logo.png" 
         alt="Easy Keto Logo" 
-        width={150}  // Adjust based on your logo dimensions
-        height={150} 
+        width="150"  // Adjust based on your logo dimensions
+        height="150" 
       />
     </div>
   );
